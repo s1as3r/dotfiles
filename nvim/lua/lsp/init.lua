3c512bd883642f13c60b7adfe0b99b1c8913043c
Arbaaz Shafiq <arbaazshafiq@gmail.com>
2021-07-22 20:38:35 +0530

    Add lsp rename mapping

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -23,6 +23,7 @@ vim.fn.sign_define("LspDiagnosticsSignInformation", {
 vim.cmd("nnoremap <silent> gd <cmd>lua vim.lsp.buf.definition()<CR>")
 vim.cmd("nnoremap <silent> gD <cmd>lua vim.lsp.buf.declaration()<CR>")
 vim.cmd("nnoremap <silent> gr <cmd>lua vim.lsp.buf.references()<CR>")
+vim.cmd("nnoremap <silent> gR <cmd>lua require('lspsaga.rename').rename()<CR>")
 vim.cmd("nnoremap <silent> gi <cmd>lua vim.lsp.buf.implementation()<CR>")
 vim.cmd("nnoremap <silent> ca :Lspsaga code_action<CR>")
 vim.cmd("nnoremap <silent> K :Lspsaga hover_doc<CR>")
