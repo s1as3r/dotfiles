574423e5add77333aca259335b5df2fd63254368
Arbaaz Shafiq <arbaazshafiq@gmail.com>
2022-09-25 16:52:25 +0530

    fish: add greeting

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,3 +1,11 @@
 function fish_greeting
-    neofetch
+    if which neofetch > /dev/null
+        neofetch
+    else
+        echo
+	    echo -e (uname -ro | awk '{print " \\\\e[1mOS: \\\\e[0;32m"$0"\\\\e[0m"}')
+	    echo -e (uptime -p | sed 's/^up //' | awk '{print " \\\\e[1mUptime: \\\\e[0;32m"$0"\\\\e[0m"}')
+	    echo -e (uname -n | awk '{print " \\\\e[1mHostname: \\\\e[0;32m"$0"\\\\e[0m"}')
+	    echo
+	end
 end
