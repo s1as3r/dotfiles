b9e20cd7dff8f0a4379afc23a92835bde29d40d9
Arbaaz Shafiq <arbaazshafiq@gmail.com>
2022-12-02 12:38:02 +0530

    nvim: add stuff

--- nvim/init.lua
+++ nvim/init.lua
@@ -39,17 +39,20 @@ local config = {
     },
     ["null-ls"] = {
       sources = {
-        -- Python
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.isort,
         null_ls.builtins.diagnostics.flake8,
         null_ls.builtins.diagnostics.mypy,
+        null_ls.builtins.diagnostics.shellcheck,
+        null_ls.builtins.diagnostics.fish,
         -- null_ls.builtins.diagnostics.pylint,
 
-        -- Bash
         null_ls.builtins.formatting.beautysh,
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.isort,
+        null_ls.builtins.formatting.jq,
+        null_ls.builtins.formatting.prettier,
       }
     },
+
 
     ["mason-lspconfig"] = {
       ensure_installed = {
@@ -60,7 +63,7 @@ local config = {
         "sumneko_lua",
         "hls",
         "gopls",
-        "ltex",
+        "texlab",
       },
     }
   },
