d58424df93fbc01722872d4724b46d254483917c
Arbaaz Shafiq <arbaazshafiq@gmail.com>
2023-04-11 07:24:27 +0530

    nvim: add forgotten cmp sources

--- nvim/plugins/user.lua
+++ nvim/plugins/user.lua
@@ -7,13 +7,15 @@ return {
     "hrsh7th/nvim-cmp",
     dependencies = {
       "kdheepak/cmp-latex-symbols",
-
     },
-
     opts = function(_, opts)
       local cmp = require "cmp"
 
       opts.sources = cmp.config.sources {
+        { name = "nvim_lsp",      priority = 1000 },
+        { name = "luasnip",       priority = 750 },
+        { name = "buffer",        priority = 500 },
+        { name = "path",          priority = 250 },
         { name = "latex_symbols", priority = 700, options = { strategy = 1 } }
       }
     end
