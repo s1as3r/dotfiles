97e2bea1e45d0d328c440d34a074fcbaaa9ed4a3
Arbaaz Shafiq <arbaazshafiq@gmail.com>
2023-08-27 13:27:33 +0530

    nvim: add timeout for formatting

--- nvim/mappings.lua
+++ nvim/mappings.lua
@@ -5,16 +5,16 @@ local maps = {
             "<cmd>.!bash<cr>",
             desc = "execute current line as bash command"
         },
-        ["<leader>T"] = {"<cmd>Telescope<cr>", desc = "Telescope"},
-        ["<C-'>"] = {"<cmd>ToggleTerm<cr>", desc = "toggle terminal"},
-        ["<C-u>"] = {"<C-u>zz"},
-        ["<C-d>"] = {"<C-d>zz"},
-        ["n"] = {"nzz"},
-        ["N"] = {"Nzz"},
+        ["<leader>T"] = { "<cmd>Telescope<cr>", desc = "Telescope" },
+        ["<C-'>"] = { "<cmd>ToggleTerm<cr>", desc = "toggle terminal" },
+        ["<C-u>"] = { "<C-u>zz" },
+        ["<C-d>"] = { "<C-d>zz" },
+        ["n"] = { "nzz" },
+        ["N"] = { "Nzz" },
         L = {
             function()
                 require("astronvim.utils.buffer").nav(vim.v.count > 0 and
-                                                          vim.v.count or 1)
+                    vim.v.count or 1)
             end,
             desc = "Next buffer"
         },
@@ -29,7 +29,7 @@ local maps = {
 }
 
 local python = vim.fn.executable("ipython") == 1 and "ipython" or
-                   vim.fn.executable("python3") == 1 and "python3"
+    vim.fn.executable("python3") == 1 and "python3"
 if python then
     maps.n["<leader>tp"] = {
         function() utils.toggle_term_cmd(python) end,
