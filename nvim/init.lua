d3b096a7f04c8f286894a6d6468da81df8d1156b
Arbaaz Shafiq <arbaazshafiq@gmail.com>
2023-10-17 20:22:54 +0530

    nvim: migrate to ibl v3

--- nvim/init.lua
+++ nvim/init.lua
@@ -102,9 +102,9 @@ require('lazy').setup({
   {
     -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
+    main = "ibl",
     opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
+      indent = { char = '┊', },
     },
   },
 
