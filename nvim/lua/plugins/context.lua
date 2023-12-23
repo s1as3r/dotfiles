007e8c2ad06c8b1053b12a3e25dcaba97f9e9e3a
Arbaaz Shafiq <arbaazshafiq@gmail.com>
2023-12-23 22:42:17 +0530

    nvim: add context plugin

---
+++ nvim/lua/plugins/context.lua
@@ -0,0 +1,18 @@
+return {
+  "nvim-treesitter/nvim-treesitter-context",
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  config = function()
+    require('treesitter-context').setup {
+      enable = true,
+      max_lines = 0,
+      min_window_height = 0,
+      line_numbers = true,
+      multiline_threshold = 20,
+      trim_scope = 'outer',
+      mode = 'cursor',
+      separator = nil,
+      zindex = 20,
+      on_attach = nil,
+    }
+  end,
+}
