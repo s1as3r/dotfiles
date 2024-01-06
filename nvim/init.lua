f8bfde024b53c72b4c754b4450a6a0ef0e7bdeb2
Arbaaz Shafiq <arbaazshafiq@gmail.com>
2024-01-06 16:35:00 +0530

    nvim: use pwsh as default shell on windows

--- nvim/init.lua
+++ nvim/init.lua
@@ -448,3 +448,15 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
+
+-- Setting Up Powershell
+-- :help shell-powershell
+if vim.loop.os_uname().sysname == "Windows_NT" then
+  vim.opt.shell = vim.fn.executable("pwsh") and "pwsh" or "powershell"
+  vim.opt.shellcmdflag =
+  "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';"
+  vim.opt.shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+  vim.opt.shellpipe = '2>&1 | %%{ "$_" } | Tee-Object %s; exit $LastExitCode'
+  vim.opt.shellquote = ""
+  vim.opt.shellxquote = ""
+end
