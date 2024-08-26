2e9c6302190ceb546e1205859c4da202426c2745
Arbaaz Shafiq <arbaazshafiq@gmail.com>
2024-08-27 01:35:05 +0530

    nvim: disable format on save

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -2,10 +2,11 @@ return {
   'stevearc/conform.nvim',
   opts = {
     notify_on_error = false,
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_fallback = true,
-    },
+    format_on_save = false,
+    -- format_on_save = {
+    --   timeout_ms = 500,
+    --   lsp_fallback = true,
+    -- },
     formatters_by_ft = {
       lua = { 'stylua' },
       python = { "isort", "black" },
