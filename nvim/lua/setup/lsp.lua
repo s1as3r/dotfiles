516ec859f2714d65e4873f1305a4ad9e25fc4b4d
Arbaaz Shafiq <arbaazshafiq@gmail.com>
2024-12-02 18:44:46 +0530

    nvim: add keymap for inlay hint toggle

--- nvim/lua/setup/lsp.lua
+++ nvim/lua/setup/lsp.lua
@@ -30,6 +30,10 @@ local on_attach = function(_, bufnr)
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, '[W]orkspace [L]ist Folders')
 
+  nmap('<leader>th', function()
+    vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = bufnr })
+  end, '[T]oggle Inlay [H]ints')
+
   -- -- Create a command `:Format` local to the LSP buffer
   -- vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
   --   vim.lsp.buf.format()
