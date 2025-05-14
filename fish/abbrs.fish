0256b14f1c18d30bf9630be697ee9f3af6dd7c62
Arbaaz Shafiq <arbaazshafiq@gmail.com>
2025-05-14 14:27:16 +0530

    fish: add some git abbrs

--- fish/abbrs.fish
+++ fish/abbrs.fish
@@ -1,32 +1,35 @@
 # aliases and abbreviations
-abbr -a py "python3"
-abbr -a ipy "ipython"
-abbr -a chx "chmod u+x"
-abbr -a v "nvim"
-abbr -a c "cargo"
-abbr -a cmd "command"
-abbr -a wwc "warp-cli connect"
-abbr -a wwd "warp-cli disconnect"
-abbr -a g "git"
-abbr -a gisc "git clone --depth=1"
-abbr -a gicl "git clone"
-abbr -a gic "git commit"
-abbr -a gis "git status"
-abbr -a gil "git log --oneline --graph"
-abbr -a ju "just"
+abbr -a py    "python3"
+abbr -a ipy   "ipython"
+abbr -a chx   "chmod u+x"
+abbr -a v     "nvim"
+abbr -a c     "cargo"
+abbr -a cmd   "command"
+abbr -a wwc   "warp-cli connect"
+abbr -a wwd   "warp-cli disconnect"
+abbr -a g     "git"
+abbr -a gisc  "git clone --depth=1"
+abbr -a gicl  "git clone"
+abbr -a gic   "git commit"
+abbr -a gis   "git status --short"
+abbr -a gil   "git log --oneline --graph"
+abbr -a gid   "git diff"
+abbr -a gids  "git diff --staged"
+abbr -a gia   "git add"
+abbr -a ju    "just"
 
 if command -v eza > /dev/null
     abbr -a ls eza
-    abbr -a l 'eza -l'
-    abbr -a ll 'eza -la'
+    abbr -a l  "eza -l"
+    abbr -a ll "eza -la"
 else
-    abbr -a l 'ls -lh'
-    abbr -a ll 'ls -lah'
+    abbr -a l  "ls -lh"
+    abbr -a ll "ls -lah"
 end
 
 if test "$TERM" = "xterm-kitty"
     abbr -a kicat 'kitty +kitten icat'
-    function hg --wraps rg; kitty +kitten hyperlinked_grep $argv; end
+    function rg --wraps rg; kitty +kitten hyperlinked_grep $argv; end
     function ssh --wraps ssh; kitty +kitten ssh $argv; end
 end
 
