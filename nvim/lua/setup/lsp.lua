4447c8aaac2f7bae745f14d87de88ecfde39cea4
Arbaaz Shafiq <arbaazshafiq@gmail.com>
2025-06-10 12:10:00 +0530

    nvim: remove dead code

--- nvim/lua/setup/lsp.lua
+++ nvim/lua/setup/lsp.lua
@@ -168,12 +168,6 @@ local servers = {
         -- Using Ruff's import organizer
         disableOrganizeImports = true,
       },
-      -- python = {
-      --   analysis = {
-      --     -- Ignore all files for analysis to exclusively use Ruff for linting
-      --     ignore = { '*' },
-      --   },
-      -- },
     }
   },
 
