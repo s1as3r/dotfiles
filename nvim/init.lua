2fe0107b514ccdc884547b2e8d3fd9cef86c0c7f
Arbaaz Shafiq <arbaazshafiq@gmail.com>
2026-01-03 19:41:05 +0530

    nvim: fix incorrect diagnostic keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -76,9 +76,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', function() vim.diagnostic.jump({ count = 1, float = true }) end,
+vim.keymap.set('n', ']d', function() vim.diagnostic.jump({ count = 1, float = true }) end,
   { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', function() vim.diagnostic.jump({ count = -1, flat = true }) end,
+vim.keymap.set('n', '[d', function() vim.diagnostic.jump({ count = -1, flat = true }) end,
   { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
