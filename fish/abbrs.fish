2373dfe31a582b35a62fa7bf6bdd9f412ba3de0d
Arbaaz Shafiq <arbaazshafiq@gmail.com>
2026-01-24 18:39:38 +0530

    fish: small config refactor

--- fish/abbrs.fish
+++ fish/abbrs.fish
@@ -18,7 +18,7 @@ abbr -a gids  "git diff --staged"
 abbr -a gia   "git add"
 abbr -a ju    "just"
 
-if command -v eza > /dev/null
+if $HAVE_EZA
     abbr -a ls eza
     abbr -a l  "eza -l"
     abbr -a ll "eza -la"
@@ -27,14 +27,18 @@ else
     abbr -a ll "ls -lah"
 end
 
-if test "$TERM" = "xterm-kitty"
+if $TERM_IS_KITTY
     abbr -a kicat 'kitten icat'
     function rg --wraps rg; kitten hyperlinked_grep $argv; end
     function ssh --wraps ssh; kitten ssh $argv; end
+    function ls --wraps ls; command ls --hyperlink $argv; end
+
+    if $HAVE_EZA
+        function eza --wraps eza; command eza --hyperlink $argv; end
+    end
 end
 
-if test (uname -o) = "Darwin"
-    # mac specefic aliases
+if $OS_IS_MAC
     abbr -a o open
 else
     abbr -a o xdg-open
--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,12 @@
+# env vars used in the config
+set TERM_IS_KITTY (test "$TERM" = "xterm-kitty"; and echo true; or echo false)
+set OS_IS_MAC     (test (uname -o) = "Darwin"; and echo true; or echo false)
+set HAVE_EZA      (command -v eza > /dev/null; and echo true; or echo false)
+
+function _source_if_exists -a fpath
+    test -f $fpath; and source $fpath
+end
+
 source $__fish_config_dir/abbrs.fish
 
 fish_add_path $HOME/.dotfiles/scripts
@@ -7,25 +16,28 @@ fish_add_path $HOME/.juliaup/bin
 fish_add_path $HOME/.local/bin
 fish_add_path /opt/cuda/bin
 
-if test (uname -o) = "Darwin"
+if $OS_IS_MAC
     fish_add_path /opt/homebrew/bin
 end
 
 # homebrew
 if test -f "/home/linuxbrew/.linuxbrew/bin/brew"
     eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+    set -gx HOMEBREW_NO_AUTO_UPDATE 1
 end
 
 # env vars
 set -gx EDITOR nvim
 set -gx FZF_DEFAULT_OPTS '--height 70% --layout=reverse --border'
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH $HOME/.ghcup/bin # ghcup-env
-set -gx HOMEBREW_NO_AUTO_UPDATE 1
+set -q  GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH $HOME/.ghcup/bin # ghcup-env
 set -gx RIPGREP_CONFIG_PATH $HOME/.ripgreprc
-set -gx ANDROID_HOME "/home/slas3r/Android/Sdk/"
 
-fish_add_path "$ANDROID_HOME/emulator"
-fish_add_path "$ANDROID_HOME/platform-tools"
+set -l _android_home "/home/slas3r/Android/Sdk/"
+if test -d $_android_home
+    set -gx ANDROID_HOME $_android_home
+    fish_add_path "$ANDROID_HOME/emulator"
+    fish_add_path "$ANDROID_HOME/platform-tools"
+end
 
 # zoxide
 if command -v zoxide > /dev/null
@@ -33,10 +45,11 @@ if command -v zoxide > /dev/null
     zoxide init fish --cmd j | source
 end
 
-# opam
-source "/home/slas3r/.opam/opam-init/init.fish" > /dev/null 2> /dev/null; or true
-
-# emsdk
-EMSDK_QUIET=1 source "/home/slas3r/.emsdk/emsdk_env.fish"
+_source_if_exists "/home/slas3r/.opam/opam-init/init.fish"
+EMSDK_QUIET=1 _source_if_exists "/home/slas3r/.emsdk/emsdk_env.fish"
 
 fish_vi_key_bindings
+
+set --erase TERM_IS_KITTY
+set --erase OS_IS_MAC
+set --erase HAVE_EZA
