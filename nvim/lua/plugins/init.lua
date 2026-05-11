02082628a14fca3419070d39262f3ef0c42081e6
Arbaaz Shafiq <arbaazshafiq@gmail.com>
2026-05-11 20:17:56 +0530

    nvim: treesitter config update for nvim 0.12
    referenced kickstart.nvim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -53,8 +53,9 @@ return {
   {
     -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
     dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
+      { 'nvim-treesitter/nvim-treesitter-textobjects', branch = 'main' },
     },
     build = ':TSUpdate',
   },
--- nvim/lua/setup/treesitter.lua
+++ nvim/lua/setup/treesitter.lua
@@ -1,60 +1,53 @@
-require('nvim-treesitter.configs').setup {
-  ensure_installed = { 'c', 'cpp', 'rust', 'python', 'bash', 'lua', 'haskell', 'go', 'latex', 'julia' },
+local ts = require('nvim-treesitter')
+local tsc = require('nvim-treesitter.config')
 
-  auto_install = true,
-
-  highlight = { enable = true },
-  indent = { enable = true },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = '<c-space>',
-      node_incremental = '<c-space>',
-      scope_incremental = '<c-s>',
-      node_decremental = '<M-space>',
-    },
-  },
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true,
-      keymaps = {
-        ['aa'] = '@parameter.outer',
-        ['ia'] = '@parameter.inner',
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
-      },
-    },
-    move = {
-      enable = true,
-      set_jumps = true,
-      goto_next_start = {
-        [']m'] = '@function.outer',
-        [']]'] = '@class.outer',
-      },
-      goto_next_end = {
-        [']M'] = '@function.outer',
-        [']['] = '@class.outer',
-      },
-      goto_previous_start = {
-        ['[m'] = '@function.outer',
-        ['[['] = '@class.outer',
-      },
-      goto_previous_end = {
-        ['[M'] = '@function.outer',
-        ['[]'] = '@class.outer',
-      },
-    },
-    swap = {
-      enable = true,
-      swap_next = {
-        ['<leader>a'] = '@parameter.inner',
-      },
-      swap_previous = {
-        ['<leader>A'] = '@parameter.inner',
-      },
-    },
-  },
+local ensure_installed = {
+  'c', 'cpp', 'rust',
+  'python', 'bash', 'lua',
+  'haskell', 'go', 'latex',
+  'julia', 'vimdoc'
 }
+local already_installed = tsc.get_installed()
+local to_install = vim.iter(ensure_installed)
+    :filter(function(parser)
+      return not vim.tbl_contains(already_installed,
+        parser)
+    end)
+    :totable()
+ts.install(to_install)
+
+local function treesitter_try_attach(buf, language)
+  if not vim.treesitter.language.add(language) then return end
+  vim.treesitter.start(buf, language)
+
+  vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+  vim.wo.foldmethod = 'expr'
+  vim.wo.foldenable = false
+
+  local has_indent_query = vim.treesitter.query.get(language, 'indents') ~= nil
+  if has_indent_query then
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  end
+end
+
+local available_parsers = require('nvim-treesitter').get_available()
+vim.api.nvim_create_autocmd('FileType', {
+  callback = function(args)
+    local buf, filetype = args.buf, args.match
+
+    local language = vim.treesitter.language.get_lang(filetype)
+    if not language then return end
+
+    local installed_parsers = require('nvim-treesitter').get_installed 'parsers'
+
+    if vim.tbl_contains(installed_parsers, language) then
+      treesitter_try_attach(buf, language)
+    elseif vim.tbl_contains(available_parsers, language) then
+      ts.install(language):await(
+        function() treesitter_try_attach(buf, language) end
+      )
+    else
+      treesitter_try_attach(buf, language)
+    end
+  end,
+})
