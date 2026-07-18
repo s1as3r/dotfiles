59b34a10d9159bbeb9250679a71a777210e45043
Arbaaz Shafiq <arbaazshafiq@gmail.com>
2026-07-18 20:33:35 +0530

    nvim: add ce snippet

--- nvim/lua/setup/snippets.lua
+++ nvim/lua/setup/snippets.lua
@@ -3,7 +3,7 @@ local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 
-local cf_snippet = s("cf", {
+local cf_off_on = s("cf", {
   t("// clang-format off"),
   t({ "", "" }),
   i(1),
@@ -11,6 +11,10 @@ local cf_snippet = s("cf", {
   t("// clang-format on"),
 })
 
+local cf_export = s("ce", {
+  i(1), t("// IWYU pragma: export")
+})
+
 for _, ft in ipairs({ "c", "cpp", "h", "hpp" }) do
-  ls.add_snippets(ft, { cf_snippet })
+  ls.add_snippets(ft, { cf_off_on, cf_export })
 end
